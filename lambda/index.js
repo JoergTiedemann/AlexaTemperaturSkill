ee07211d31d5a126c98b241291ce67ba9e6a6048
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2025-01-15 14:32:37 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -97,12 +97,12 @@ const GetTemperatureIntentHandler = {
             //firebase.database().goOnline();
             await signInWithEmail();
             console.log(`~~~~ firebase goOnline erfolgt`);
-/*            
+            
             const dbRef = database.ref();
             await dbRef.child('/Heizung/').child('/Heizungsmonitor/').child('/Heizungstatus/').child('/aktuelleTemp/').get().then((snapshot) => {
                 if (snapshot.exists()) {
                 console.log('~~~~~ der Wert ist:',snapshot.val());
-                speakOutput = `Die Temperatur beträgt ${snapshot.val()} Grad `;
+//                speakOutput = `Die Temperatur beträgt ${snapshot.val()} Grad `;
 
                 } else {
                     console.log("No data available");
@@ -110,7 +110,7 @@ const GetTemperatureIntentHandler = {
             }).catch((error) => {
                 console.error(error);
                 });
-*/
+
   /*          console.log(`~~~~ firebase schreiben start`);
 
             await database.ref('/Messwerte/').set({
