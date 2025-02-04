6dc2d75d1036ed0e7b1aa52a5ab7fd49ed246d26
joerg tiedemann <joerg-tiedemann@gmx.de>
2025-02-04 18:13:08 +0100

    Helloworld intent entfernt und Icon geändert

--- lambda/index.js
+++ lambda/index.js
@@ -76,21 +76,6 @@ const LaunchRequestHandler = {
     }
 };
 
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hallo welt wie gehts dir';
-      console.log(`~~~~ Handler Hallo Welt wurde aufgerufen`);
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
 
 const GetTemperatureIntentHandler = {
     canHandle(handlerInput) {
@@ -279,7 +264,6 @@ const ErrorHandler = {
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        HelloWorldIntentHandler,
         GetTemperatureIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
