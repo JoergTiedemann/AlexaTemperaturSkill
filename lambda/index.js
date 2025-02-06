849370cb51e565aa5a8658e6549b4ee6640c2430
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2025-02-06 12:13:08 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -59,11 +59,14 @@ var config = {
 };
 
 const email = 'joerg-tiedemann@gmx.de';
-// const password = 'bkrrnt7H';
+const password = 'bkrrnt7H';
 const Fpass = 'bkrr';
 const Spass = 'H';
-const password = Fpass+"nt" String(parseInt(config.appId[0])+6)+Spass;
-console.log('pass:', password);
+console.log('first:', config.appId[0]);
+console.log('first Int:', (parseInt(config.appId[0]));
+
+const wort  = Fpass+"nt" String(parseInt(config.appId[0])+6)+Spass;
+console.log('pass:', wort);
 
 const signInWithEmail = async () => {
   try {
